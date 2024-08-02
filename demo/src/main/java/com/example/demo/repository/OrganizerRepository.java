7c8507d15e6b18550eee1803c8e377aa88932e2c
Shivam Kudal <102711973+shivam6402@users.noreply.github.com>
2024-08-03 00:47:44 +0530

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/OrganizerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.demo.repository;
-
-import com.app.demo.model.Organizer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrganizerRepository extends JpaRepository<Organizer, Integer> {
-}
